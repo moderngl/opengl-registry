2d6b8a1cdf531198ce4fb68cec031d95ef1d00c0
einarf <eforselv@gmail.com>
2019-09-14 23:23:04 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="opengl-api-registry-reader",
+    author="Einar Forselv",
+    author_email="eforselv@gmail.com",
+    description="",
+    install_requires=[
+        'requests<3',
+    ],
+)
